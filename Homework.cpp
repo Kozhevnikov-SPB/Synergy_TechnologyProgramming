e6f6abb5f49c7eaf43fa87db2ce06715f9660db4
Konstantin Kozhevnikov <mail@kozhevnikovspb.ru>
2023-03-16 23:43:10 +0300

    Task02

--- Homework.cpp
+++ Homework.cpp
@@ -2,14 +2,14 @@
 
 int main()  {
 	
-	int pin1, pin2;
+	long long int pin1, pin2;
 
 	printf_s("pin1 = ");
-	scanf_s("%i", &pin1);
+	scanf_s("%lli", &pin1);
 	printf_s("pin2 = ");
-	scanf_s("%i", &pin2);
+	scanf_s("%lli", &pin2);
 
-	if ((pin1 == 111) && (pin2 == 221))	{
+	if (((pin1 == 4111) && (pin2 == 3221)) || ((pin1 == 5331) && (pin2 == 6531))) {
 		printf("OK");
 	}
 	else {	
