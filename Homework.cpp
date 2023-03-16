038e84b981945f5d98c81b6ac73a60395bb4834b
Konstantin Kozhevnikov <mail@kozhevnikovspb.ru>
2023-03-16 22:53:06 +0300

    Homework 2.2

--- Homework.cpp
+++ Homework.cpp
@@ -1,5 +1,23 @@
 ﻿#include <stdio.h>
 
 int main()  {
-    printf("Hello");
+	
+	int a, b;
+
+	printf_s("A = ");
+	scanf_s("%i", &a);
+	printf_s("B = ");
+	scanf_s("%i", &b);
+
+	if (a > b) {
+		printf("Больше");
+	}
+	else {
+		if (a == b) {
+			printf("Равны");
+		}
+		else {
+			printf("Меньше");
+		}		
+	}
 }
