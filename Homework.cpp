643654c1f75ac238d840f5ebcd60a766aef4d965
Konstantin Kozhevnikov <mail@kozhevnikovspb.ru>
2023-03-19 12:27:48 +0300

    prog 2 in Task 01

--- Homework.cpp
+++ Homework.cpp
@@ -1,12 +1,22 @@
-﻿//Ссылка на программу в репозитории - https://github.com/Kozhevnikov-SPB/Synergy_TechnologyProgramming/tree/Work_03_05_01_1
+﻿//Ссылка на программу в репозитории - https://github.com/Kozhevnikov-SPB/Synergy_TechnologyProgramming/tree/Work_03_05_01_2
 #include <stdio.h>
 
 
 int main() {
 	
 	int i = 0;
-	while (i < 9)	{
-		printf("Hello\n");
+
+	while (i < 15)	{		
+		printf("%i ", i);			//Вывожу числа от 0 с шагом 1 с количеством итераций 15. Вывод в строчку через пробел		
 		i++;
+	}
+
+	printf("\n");					//Делаю перевод каретки на новую строчку
+
+	i = 2;
+
+	while (i < 15)	{
+		printf("%i ", i);			//Вывожу только четные числа. Вывод в строчку через пробел
+		i += 2;
 	}
 }
