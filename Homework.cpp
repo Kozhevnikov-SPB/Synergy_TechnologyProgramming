9374fccfc0133a24446816ea8209ba07a217b28b
Konstantin Kozhevnikov <mail@kozhevnikovspb.ru>
2023-03-19 13:53:31 +0300

    add task 02

--- Homework.cpp
+++ Homework.cpp
@@ -1,10 +1,25 @@
-﻿//Ссылка на программу в репозитории - https://github.com/Kozhevnikov-SPB/Synergy_TechnologyProgramming/tree/Work_03_06_01
+﻿//Ссылка на программу в репозитории - https://github.com/Kozhevnikov-SPB/Synergy_TechnologyProgramming/tree/Work_03_06_02
 #include <stdio.h>
 
 
 int main() {
 	
-	for (int i = 1; i < 11; i++) {
-		printf("5 * %i = %i\n", i, i * 5);
-	}
+	int a;
+
+	do {
+		printf("Ugadai chislo - ");
+		scanf_s("%i", &a);
+		if (a == 7) {
+			printf("Chislo ugadano!\n");
+			break;
+		}
+		else {
+			if (a > 7)		printf("Bolshe\n");			
+			if (a < 7)		printf("Menshe\n");			
+			if (a > 10)		printf("Vvedenoe chislo bolshe chem 10\n");			
+			if (a < 10)		printf("Vvedenoe chislo menshe chem 10\n");			
+			if (a % 2 == 0)	printf("Vvedenoe chislo delitsia na 2\n");			
+			if (a % 3 == 0)	printf("Vvedenoe chislo delitsia na 3\n");			
+		}
+	} while (true);
 }
