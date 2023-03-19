99edc44a336ede34706c24d818fa797e39585a0e
Konstantin Kozhevnikov <mail@kozhevnikovspb.ru>
2023-03-19 12:11:30 +0300

    prog 1 in Task 01

--- Homework.cpp
+++ Homework.cpp
@@ -1,25 +1,12 @@
-﻿//Ссылка на программу в репозитории - https://github.com/Kozhevnikov-SPB/Synergy_TechnologyProgramming/tree/Work_02_Final
+﻿//Ссылка на программу в репозитории - https://github.com/Kozhevnikov-SPB/Synergy_TechnologyProgramming/tree/Work_03_05_01_1
 #include <stdio.h>
 
 
-int main()  {
+int main() {
 	
-	int a = 1, sum = 0;
-
-	while (a < 3) {		
-		sum = sum + a;		
-		printf("%i+", a);
-		a++;		
+	int i = 0;
+	while (i < 9)	{
+		printf("Hello\n");
+		i++;
 	}
-	printf(" Sum = %i\n", sum);
-
-	a = 0;
-	sum = 0;
-
-	while (a < 3) {		
-		sum = sum + a;			
-		printf("%i+", a);
-		a++;		
-	}
-	printf(" Sum = %i\n", sum);
 }
